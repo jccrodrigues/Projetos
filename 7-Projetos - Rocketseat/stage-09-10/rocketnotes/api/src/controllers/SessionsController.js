3273295d6732e63e0f3ffb9568b60522a3cdce35
jccrodrigues <jccrprofissional@gmail.com>
2023-09-13 21:28:34 -0300

    Atualização

---
+++ 7-Projetos - Rocketseat/stage-09-10/rocketnotes/api/src/controllers/SessionsController.js
@@ -0,0 +1,41 @@
+const knex = require("../database/knex");
+const { compare } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+const { sign } = require("jsonwebtoken");
+const authConfig = require("../configs/auth");
+
+class SessionsController {
+  async create(request, response) {
+    const { email, password } = request.body;
+
+
+    //Linha de código verifica se o usuário existe no banco
+    const user = await knex("users").where({ email }).first();
+
+    if (!user) {
+      throw new AppError("E-mail e/ou senha incorreta.", 401);
+    }
+
+
+    //Verifica se a senha digitada está correta
+
+    const passwordMatched = await compare(
+      password,
+      user.password,
+    );
+
+    if (!passwordMatched) {
+      throw new AppError("E-mail e/ou senha incorreta.", 401);
+    }
+
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn
+    });
+
+    response.status(201).json({ token, user });
+  }
+}
+
+module.exports = SessionsController;
